77d4dd7d896ae181eef22b53a91bce1f2dcb6077
hellokack <eogeo85@gmail.com>
2025-07-09 23:46:35 +0900

    Title: 배열 채우기, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250709/배열 채우기/filling-array.py
@@ -0,0 +1,24 @@
+hungry = []
+num=[]
+for i in range (10):
+    n = list(map(int, input().split()))
+    if n == 0:
+        break
+    else:
+        hungry.append(n)
+# for i in range(10):
+#     try:
+#         n = input()
+#     except EOFError:
+#         break
+#     if n == "0":
+#         break
+#     else:
+#         hungry.append(n)
+
+
+# for i in range(10,-1,-1):
+#     hungry.append(num[i])
+# print(hungry)
+    
+    
