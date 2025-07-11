fae7b6f7b91a6ac870106fd816a86301162a25c2
hellokack <eogeo85@gmail.com>
2025-07-11 22:06:03 +0900

    Title: 숫자들의 배수, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250711/숫자들의 배수/multiple-of-numbers.py
+++ 250711/숫자들의 배수/multiple-of-numbers.py
@@ -1,14 +1,11 @@
 arr=list(map(int, input()))
-arr= [arr[0]*i for i in range(1,11)]
+arr= [arr[0]*i for i in range(1,30)]
 cnt=0
 arr2=[]
 for i in arr:
-    # print(i, end=' ')
-    arr2.append(i)
+    print(i, end=' ')
     if i %5 ==0:
         cnt+=1
     if cnt == 2:
         break
-for i in arr2:
-    print(i, end=' ')
     
