6309a58b5c1b4dd7312c36bb181467e7d5ff8aa1
hellokack <eogeo85@gmail.com>
2025-07-15 23:14:32 +0900

    Title: 배열 놀이, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250715/배열 놀이/play-with-array.py
@@ -0,0 +1,16 @@
+n,q=map(int,input().split())
+n_list=list(map(int,input().split()))
+for j in range(q):
+    q_list=list(map(int,input().split()))
+    if q_list[0]==1:
+        print(n_list[q_list[1]-1])
+    elif q_list[0]==2:
+        if q_list[1] not in n_list:
+            print("0")
+        else:
+            print(n_list.index(q_list[1])+1)
+    elif q_list[0]==3:
+        for i in range(q_list[1]-1,q_list[2]):
+            print(n_list[i],end= ' ')
+        print()
+                
