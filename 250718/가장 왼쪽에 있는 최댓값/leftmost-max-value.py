ed694f9beb48fb01f94eb19efc9ecc8f49bb5fde
hellokack <eogeo85@gmail.com>
2025-07-18 14:41:21 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250718/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,35 @@
+import sys
+n = int(input())
+a = list(map(int, input().split()))
+b=[]
+# Please write your code here.
+
+# 
+# 0. while(index != 0)
+# 1. 배열(0 ~ index)의 최댓값 max의 idx를 찾는 과정
+# 2. 0 ~ idx를 배열 A로 쳐서 다시 while문
+max_a = len(a)
+while max_a != -1:
+    idx = 0
+    for i in range(max_a) :
+        # 최댓값 idx 찾기
+        if a[idx] < a[i] :
+            idx = i
+    
+    print(idx + 1, end=" ")
+    max_a = idx - 1
+
+# max=-sys.maxsize
+# for i in a:
+#     if i > max:
+#         max= i
+# while max!=1: 
+#     for i in range (10):
+#         for j in range((a.index(max))+1):
+#             b.append(a[j])
+#             print(b)
+#         max=-sys.maxsize
+#         for i in b:
+#             if i > max:
+#                 max= i
+# print(b)
