c452695bb0b30b116407f8f971b0553b6349ef4a
hellokack <eogeo85@gmail.com>
2025-07-26 22:30:38 +0900

    Title: 비교 연산, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250726/비교 연산/comparison-operator.py
@@ -0,0 +1,27 @@
+arr=list(map(int,input().split()))
+a=arr[0]
+b=arr[1]
+if a>=b:
+    print(1)
+else:
+    print(0)
+if a>b:
+    print(1)
+else:
+    print(0)
+if a<=b:
+    print(1)
+else:
+    print(0)
+if a<b:
+    print(1)
+else:
+    print(0)
+if a==b:
+    print(1)
+else:
+    print(0)
+if a!=b:
+    print(1)
+else:
+    print(0)
