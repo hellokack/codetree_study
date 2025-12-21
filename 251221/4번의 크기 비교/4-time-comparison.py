856ae9b6cfa4be6fc44473c0d37e3a7740a6fbed
hellokack <eogeo85@gmail.com>
2025-12-21 23:26:38 +0900

    Title: 4번의 크기 비교, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251221/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,18 @@
+a=int(input())
+arr=list(map(int,input().split()))
+if a>arr[0]:
+    print(1)
+else:
+    print(0)
+if a>arr[1]:
+    print(1)
+else:
+    print(0)
+if a>arr[2]:
+    print(1)
+else:
+    print(0)
+if a>arr[3]:
+    print(1)
+else:
+    print(0)
