6e535f71ee244a8092d66f534a7f0e6208ebacc5
hellokack <eogeo85@gmail.com>
2026-01-04 01:01:19 +0900

    Title: 두 사람, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260104/두 사람/two-person.py
+++ 260104/두 사람/two-person.py
@@ -1,7 +1,8 @@
 a=list(input().split())
 b=list(input().split())
-if int(a[0])>=19 and int(b[0])>=19:
-    if a[1]=="M" or b[1]=="M":
-        print(1)
-    else:
-        print(0)
+if int(a[0])>=19 or a[1]=="M":
+    print(1)
+elif int(b[0])>=19 or b[1]=="M":
+    print(1)
+else:
+    print(0)
