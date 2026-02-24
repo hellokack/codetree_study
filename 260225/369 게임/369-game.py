a4e8c1d2e3480c22d7252457bec2e1be16214439
hellokack <eogeo85@gmail.com>
2026-02-25 00:06:08 +0900

    Title: 369 게임, Time: 64ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260225/369 게임/369-game.py
@@ -0,0 +1,10 @@
+a=int(input())
+i=1
+while i<=a:
+    if i%10==3 or i%10==6 or i%10==9:
+        print(0,end=" ")
+    elif i%3==0:
+        print(0,end=" ")
+    else:
+        print(i,end=" ")
+    i+=1
