36227662a40ba5716ed9c7f8f67d7c314874b4ce
hellokack <eogeo85@gmail.com>
2026-02-25 00:11:20 +0900

    Title: 369 게임, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260225/369 게임/369-game.py
+++ 260225/369 게임/369-game.py
@@ -1,7 +1,9 @@
 a=int(input())
 i=1
 while i<=a:
-    if i%10==3 or i%10==6 or i%10==9:
+    if i//10==3 or i//10==6 or i//10==9:
+        print(0,end=" ")  
+    elif i%10==3 or i%10==6 or i%10==9:
         print(0,end=" ")
     elif i%3==0:
         print(0,end=" ")
